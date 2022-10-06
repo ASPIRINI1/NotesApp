ca4ac605004d40c7fa714111820f171fa9e12cf6
Stanislav Zverkov <zverk3333@mail.ru>
2022-10-06 17:22:48 +0300

    TabBar configuration code replaced.

--- NotesApp/SceneDelegate.swift
+++ NotesApp/SceneDelegate.swift
@@ -15,21 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        
-        let tabBarController = TabBarController()
-        let notesTableNavigationController = UINavigationController(rootViewController: ModuleBuilder.createNotesTable())
-        let settingsNavigationController = UINavigationController(rootViewController: ModuleBuilder.createSettingsTableViewController())
-        
-        notesTableNavigationController.title = NSLocalizedString("Notes table", comment: "")
-        notesTableNavigationController.viewControllers.first?.navigationItem.title = NSLocalizedString("Notes table", comment: "")
-        notesTableNavigationController.tabBarItem.image = UIImage(systemName: "note.text")
-        
-        settingsNavigationController.title = NSLocalizedString("Settings", comment: "")
-        settingsNavigationController.viewControllers.first?.navigationItem.title = NSLocalizedString("Settings", comment: "")
-        settingsNavigationController.tabBarItem.image = UIImage(systemName: "gear")
-        
-        tabBarController.setViewControllers([notesTableNavigationController, settingsNavigationController], animated: true)
-        window?.rootViewController = tabBarController
+        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
     }
 }
--- NotesApp/TabBarController.swift
+++ NotesApp/TabBarController.swift
@@ -11,6 +11,20 @@ class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let notesTableNavigationController = UINavigationController(rootViewController: ModuleBuilder.createNotesTable())
+        let settingsNavigationController = UINavigationController(rootViewController: ModuleBuilder.createSettingsTableViewController())
+        
+        notesTableNavigationController.title = NSLocalizedString("Notes table", comment: "")
+        notesTableNavigationController.viewControllers.first?.navigationItem.title = NSLocalizedString("Notes table", comment: "")
+        notesTableNavigationController.tabBarItem.image = UIImage(systemName: "note.text")
+        
+        settingsNavigationController.title = NSLocalizedString("Settings", comment: "")
+        settingsNavigationController.viewControllers.first?.navigationItem.title = NSLocalizedString("Settings", comment: "")
+        settingsNavigationController.tabBarItem.image = UIImage(systemName: "gear")
+        
+        setViewControllers([notesTableNavigationController, settingsNavigationController], animated: true)
+        
         switch AppSettings.shared.appTheme {
             case 0: self.overrideUserInterfaceStyle = .unspecified
             case 1: self.overrideUserInterfaceStyle = .light
